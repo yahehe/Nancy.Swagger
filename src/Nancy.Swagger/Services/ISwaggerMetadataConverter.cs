e81b4ab390ea6249b9573b5e9105598f288037f4
Tim Schlechter <timschlechter@gmail.com>
2014-07-15 21:41:06 +0200

    Inject IRouteCacheProvider in DefaultSwaggerMetadataConverter instead of SwaggerModule

--- src/Nancy.Swagger/Services/ISwaggerMetadataConverter.cs
+++ src/Nancy.Swagger/Services/ISwaggerMetadataConverter.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Generic;
-using Swagger.Model.ApiDeclaration;
+﻿using Swagger.Model.ApiDeclaration;
 using Swagger.Model.ResourceListing;
 
 namespace Nancy.Swagger.Services
 {
     public interface ISwaggerMetadataConverter
     {
-        ResourceListing GetResourceListing(IEnumerable<SwaggerRouteData> routeData);
+        ResourceListing GetResourceListing();
 
-        ApiDeclaration GetApiDeclaration(IEnumerable<SwaggerRouteData> routeData);
+        ApiDeclaration GetApiDeclaration(string resourcePath);
     }
 }
