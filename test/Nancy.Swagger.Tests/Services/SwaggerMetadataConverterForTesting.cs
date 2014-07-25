0d5549c9c969787724d620fa4f22c45b7deedbc8
Jim Liddell <jim.liddell@linn.co.uk>
2014-07-25 09:06:19 +0100

    Added some more tests.

---
+++ test/Nancy.Swagger.Tests/Services/SwaggerMetadataConverterForTesting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy.Swagger.Services;
+
+namespace Nancy.Swagger.Tests.Services
+{
+    public class SwaggerMetadataConverterForTesting : SwaggerMetadataConverter
+    {
+        public SwaggerMetadataConverterForTesting()
+        {
+            RouteDataAccessor = Enumerable.Empty<SwaggerRouteData>();
+            ModelDataAccessor = Enumerable.Empty<SwaggerModelData>();
+        }
+
+        public IEnumerable<SwaggerRouteData> RouteDataAccessor { get; set; }
+
+        public IEnumerable<SwaggerModelData> ModelDataAccessor { get; set; }
+
+        protected override IEnumerable<SwaggerRouteData> RetrieveSwaggerRouteData()
+        {
+            return RouteDataAccessor;
+        }
+
+        protected override IEnumerable<SwaggerModelData> RetrieveSwaggerModelData()
+        {
+            return ModelDataAccessor;
+        }
+    }
+}
---
+++ test/Nancy.Swagger.Tests/SwaggerModelPropertyDataTests.cs
@@ -0,0 +1,22 @@
+﻿using Should;
+using Xunit;
+
+namespace Nancy.Swagger.Tests
+{
+    public class SwaggerModelPropertyDataTests
+    {
+        private SwaggerModelPropertyData propertyData;
+
+        public SwaggerModelPropertyDataTests()
+        {
+            propertyData = new SwaggerModelPropertyData();
+        }
+
+        [Fact]
+        public void ShouldSetEnumToNullByDefault()
+        {
+            // Assert
+            propertyData.Enum.ShouldBeNull();
+        }
+    }
+}
