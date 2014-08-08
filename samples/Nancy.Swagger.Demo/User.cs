259c1333a3d176440bf645503b19fee345221642
Kristian Hellang <kristian@hellang.com>
2014-08-08 13:31:11 +0200

    Moved Nancy.Swagger.Demo

---
+++ samples/Nancy.Swagger.Demo/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Nancy.Swagger.Demo
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 100)]
+        public int Age { get; set; }
+    }
+}
---
+++ samples/Nancy.Swagger.Demo/UserModelDataProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace Nancy.Swagger.Demo
+{
+    using Nancy.Swagger.Services;
+
+    public class UserModelDataProvider : ISwaggerModelDataProvider
+    {
+        public SwaggerModelData GetModelData()
+        {
+            return SwaggerModelData.ForType<User>(with =>
+            {
+                with.Description("A user of our awesome system!");
+                with.Property(x => x.Name)
+                    .Description("The user's name")
+                    .Required(true);
+                with.Property(x => x.Age)
+                    .Description("The user's age")
+                    .Required(true)
+                    .Minimum(1)
+                    .Maximum(100);
+            });
+        }
+    }
+}
