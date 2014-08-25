366b0f98cf2ace22c2644698716605fa5d1f78d1
Kristian Hellang <kristian@hellang.com>
2014-08-25 23:03:55 +0200

    Updated version

--- etc/CommonAssemblyInfo.cs
+++ etc/CommonAssemblyInfo.cs
@@ -5,4 +5,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha2")]
