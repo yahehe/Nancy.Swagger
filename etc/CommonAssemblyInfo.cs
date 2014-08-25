a2245abdf67ead8316e2bd98808cff0cf544bca8
Kristian Hellang <kristian@hellang.com>
2014-08-25 23:14:18 +0200

    AssemblyInfo updates

--- etc/CommonAssemblyInfo.cs
+++ etc/CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyCompany("khellang")]
+[assembly: AssemblyCompany("Kristian Hellang and Contributors")]
 [assembly: AssemblyCopyright("Copyright © Kristian Hellang 2014")]
 
 [assembly: AssemblyVersion("0.1.0")]
--- src/Nancy.Swagger.Annotations/Properties/AssemblyInfo.cs
+++ src/Nancy.Swagger.Annotations/Properties/AssemblyInfo.cs
@@ -4,5 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Nancy.Swagger.Annotations")]
 [assembly: AssemblyDescription("Provides Swagger data by annotating modules and routes.")]
 [assembly: AssemblyProduct("Nancy.Swagger.Annotations")]
+
 [assembly: ComVisible(false)]
+
 [assembly: Guid("5e104aee-12f5-46dd-9ab0-0c47e0ff0def")]
