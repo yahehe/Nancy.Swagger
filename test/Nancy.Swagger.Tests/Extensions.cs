b50ce2c0af9b3fef3e245c860d1989d80323e712
Adam Hathcock <adam.hathcock@autodesk.com>
2015-05-12 13:07:35 +0100

    Remove invalid tests

--- test/Nancy.Swagger.Tests/Extensions.cs
+++ test/Nancy.Swagger.Tests/Extensions.cs
@@ -1,6 +1,5 @@
 ﻿using Should;
 using Swagger.ObjectModel;
-using Swagger.ObjectModel;
 
 namespace Nancy.Swagger.Tests
 {
@@ -8,7 +7,7 @@ namespace Nancy.Swagger.Tests
     {
         public static void ShouldEqual(this DataType actual, DataType expected, string userMessage = null)
         {
-            actual.DefaultValue.ShouldEqual(expected.DefaultValue, userMessage.Append("DefaultValue"));
+            actual.Default.ShouldEqual(expected.Default, userMessage.Append("Default"));
             actual.Enum.ShouldEqual(expected.Enum, userMessage.Append("Enum"));
             actual.Format.ShouldEqual(expected.Format, userMessage.Append("Format"));
             actual.Items.ShouldEqual(expected.Items, userMessage.Append("Items"));
@@ -17,31 +16,6 @@ namespace Nancy.Swagger.Tests
             actual.Ref.ShouldEqual(expected.Ref, userMessage.Append("Ref"));
             actual.Type.ShouldEqual(expected.Type, userMessage.Append("Type"));
             actual.UniqueItems.ShouldEqual(expected.UniqueItems, userMessage.Append("UniqueItems"));
-        }
-
-        public static void ShouldEqual(this Parameter actual, Parameter expected, string userMessage = null)
-        {
-            (actual as DataType).ShouldEqual(expected, userMessage);
-
-            actual.Description.ShouldEqual(expected.Description, userMessage.Append("DefaultValue"));
-            actual.Name.ShouldEqual(expected.Name, userMessage.Append("Name"));
-            actual.In.ShouldEqual(expected.In, userMessage.Append("ParamType"));
-            actual.Required.ShouldEqual(expected.Required, userMessage.Append("Required"));
-        }
-
-        public static void ShouldEqual(this Operation actual, Operation expected, string userMessage = null)
-        {
-            (actual as DataType).ShouldEqual(expected, userMessage);
-
-            actual.Authorizations.ShouldEqual(expected.Authorizations, userMessage.Append("Authorizations"));
-            actual.Consumes.ShouldEqual(expected.Consumes, userMessage.Append("Consumes"));
-            actual.Deprecated.ShouldEqual(expected.Deprecated, userMessage.Append("Deprecated"));
-            actual.Method.ShouldEqual(expected.Method, userMessage.Append("Method"));
-            actual.Description.ShouldEqual(expected.Description, userMessage.Append("Notes"));
-            actual.OperationId.ShouldEqual(expected.OperationId, userMessage.Append("Nickname"));
-            actual.Parameters.ShouldEqual(expected.Parameters, userMessage.Append("Parameters"));
-            actual.Produces.ShouldEqual(expected.Produces, userMessage.Append("Produces"));
-            actual.Summary.ShouldEqual(expected.Summary, userMessage.Append("Summary"));
         }
 
         public static void ShouldEqual(this ModelProperty actual, ModelProperty expected, string userMessage = null)
