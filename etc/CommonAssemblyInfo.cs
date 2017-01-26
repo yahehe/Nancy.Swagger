04e74391367225e6d687c2d4c10337fa2263c8c3
Ciaran Downey <cdowney@cimpress.com>
2017-01-26 13:21:28 -0500

    Update version to 2.1.1-alpha

--- etc/CommonAssemblyInfo.cs
+++ etc/CommonAssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Kristian Hellang and Contributors")]
 [assembly: AssemblyCopyright("Copyright © Kristian Hellang 2014")]
 
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0-alpha")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyInformationalVersion("2.1.1-alpha")]
