9c69aefbb932950141e71d04edfbc42334592d3d
jnallard <jnallard@wpi.edu>
2017-04-13 21:22:24 -0400

    Updates packing

--- nugetPackageAll.bat
+++ nugetPackageAll.bat
@@ -1,8 +1,5 @@
 echo %APPVEYOR_BUILD_NUMBER%
-powershell -Command "(gc src/Nancy.Swagger/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Nancy.Swagger/project.json"
-powershell -Command "(gc src/Nancy.Swagger.Annotations/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Nancy.Swagger.Annotations/project.json"
-powershell -Command "(gc src/Swagger.ObjectModel/project.json) -replace '.0-\*', '.%APPVEYOR_BUILD_NUMBER%-*' | Out-File src/Swagger.ObjectModel/project.json"
 
-dotnet pack src/Nancy.Swagger --configuration Release --output src/Nancy.Swagger/NuGet --version-suffix "alpha"
-dotnet pack src/Nancy.Swagger.Annotations --configuration Release --output src/Nancy.Swagger.Annotations/NuGet --version-suffix "alpha"
-dotnet pack src/Swagger.ObjectModel --configuration Release --output src/Swagger.ObjectModel/NuGet --version-suffix "alpha"
+dotnet pack src/Nancy.Swagger --configuration Release --version-suffix "alpha"
+dotnet pack src/Nancy.Swagger.Annotations --configuration Release --version-suffix "alpha"
+dotnet pack src/Swagger.ObjectModel --configuration Release --version-suffix "alpha"
