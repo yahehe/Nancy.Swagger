0eb4c89c2d8f0217fc0f343099d0c4053dacc085
catcherwong <catcher_hwq@outlook.com>
2017-10-13 23:01:51 +0800

    add some bulider tests

---
+++ test/Swagger.ObjectModel.Tests/Builders/LicenseBuilderTest.cs
@@ -0,0 +1,27 @@
+﻿using Swagger.ObjectModel.Builders;
+using Xunit;
+
+namespace Swagger.ObjectModel.Tests.Builders
+{
+    public class LicenseBuilderTest
+    {                     
+        [Theory]
+        [InlineData("")]        
+        [InlineData(null)]
+        public void Should_ThrowRequiredFieldException_When_NameIsNullOrWhiteSpace(string name)
+        {            
+            Assert.Throws(typeof(RequiredFieldException), () => new LicenseBuilder(name).Build());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("https://github.com/yahehe/Nancy.Swagger")]
+        public void Should_AbleToSetUrl(string url)
+        {
+            string name = "MIT";
+            var license = new LicenseBuilder(name).Url(url).Build();
+
+            Assert.Equal(url, license.Url);
+        }        
+    }
+}
---
+++ test/Swagger.ObjectModel.Tests/Builders/TagBuilderTest.cs
@@ -0,0 +1,73 @@
+﻿using Swagger.ObjectModel.Builders;
+using Xunit;
+
+namespace Swagger.ObjectModel.Tests.Builders
+{
+    public class TagBuilderTest
+    {
+        private readonly TagBuilder builder;
+        private readonly string name = string.Empty;
+
+        public TagBuilderTest()
+        {
+            this.builder = new TagBuilder();
+            this.name = "name";
+        }
+
+        [Fact]
+        public void Should_ThrowRequiredFieldException_When_NameIsNotSet()
+        {
+            Assert.Throws(typeof(RequiredFieldException), () => builder.Build());            
+        }
+
+
+        [Fact]
+        public void Should_ThrowRequiredFieldException_When_NameIsNullOrWhiteSpace()
+        {
+            Assert.Throws(typeof(RequiredFieldException), () => builder.Name(null).Build());
+            Assert.Throws(typeof(RequiredFieldException), () => builder.Name("").Build());
+        }
+
+        [Fact]
+        public void Should_AbleToSetName()
+        {            
+            var tag = builder.Name(name).Build();
+
+            Assert.Equal(name, tag.Name);
+        }
+
+        [Fact]
+        public void Should_AbleToSetDescription()
+        {
+            string description = "description";
+
+            var tag = builder.Name(name).Description(description).Build();
+
+            Assert.Equal(description, tag.Description);
+        }
+
+        [Fact]
+        public void Should_AbleToSetExternalDocumentation()
+        {
+            var externalDocumentation = new ExternalDocumentation();        
+
+            var tag = builder.Name(name)
+                             .ExternalDocumentation(externalDocumentation)
+                             .Build();
+            
+            Assert.Equal(externalDocumentation, tag.ExternalDocumentation);            
+        }
+
+        [Fact]
+        public void Should_AbleToSetExternalDocumentationWithBuilder()
+        {
+            var exBuilder = new ExternalDocumentationBuilder().Url("url") ;
+
+            var tag = builder.Name(name)
+                             .ExternalDocumentation(exBuilder)
+                             .Build();
+
+            Assert.Equal("url", tag.ExternalDocumentation.Url);
+        }
+    }
+}
