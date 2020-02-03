c300f199c9589b20e4ffdcf9d9160bd0849836a5
See Kiong <seekiong.shee@appspace.com>
2020-02-03 13:36:32 +0800

    Added sample for feature support-of-annotating-class-object-for-querystring-routeparam

---
+++ samples/Nancy.Swagger.Annotations.Demo/Models/CafeMenuQuery.cs
@@ -0,0 +1,34 @@
+﻿using Nancy.Swagger.Annotations.Attributes;
+
+namespace Nancy.Swagger.Annotations.Demo.Models
+{
+    [Model("Querystring for cafe menu")]
+    public class CafeMenuQuery
+    {
+        [ModelProperty(Description = "for paging", Required = false, Minimum = 0, Maximum = 20)]
+        public int Page { get; set; }
+
+        [ModelProperty(Description = "for paging", Required = false, Minimum = 0, Maximum = 20)]
+        public int PageCount { get; set; }
+
+        [ModelProperty(Description = "field to sort, available field: name, price, category", Required = false)]
+        public string Sort { get; set; }
+
+        [ModelProperty(Description = "sorting direction, asc or desc", Required = false)]
+        public string SortDirection { get; set; }
+
+        [ModelProperty(Description = "query for item that has price higher than this", Required = false)]
+        public double MinumumPrice { get; set; }
+
+        [ModelProperty(Description = "query for item that has price lower than this", Required = false)]
+        public double MaximumPrice { get; set; }
+
+        [ModelProperty(Description = "query for item that is vegan", Required = false)]
+        public bool? VeganOnly { get; set; }
+
+        [ModelProperty(Description = "query for item that belongs to the specific category", Required = false)]
+        public MenuCategory Category { get; set; }
+    }
+
+
+}
---
+++ samples/Nancy.Swagger.Annotations.Demo/Modules/CafeModule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Swagger.Annotations.Attributes;
+using Nancy.Swagger.Annotations.Demo.Models;
+using Swagger.ObjectModel;
+
+namespace Nancy.Swagger.Annotations.Demo.Modules
+{
+    public class CafeModule : NancyModule
+    {
+        public CafeModule(ISwaggerModelCatalog modelCatalog) : base("/cafe")
+        {
+            modelCatalog.AddModel<CafeMenu>();
+            modelCatalog.AddModel<MenuItem>();
+
+            Get(
+                "/menu", 
+                parameter =>
+                {
+                    var cafeMenuQuery = new CafeMenuQuery
+                    {
+                        Page = Request.Query.page ?? 1,
+                        PageCount = Request.Query.pagecount ?? 10,
+                        Sort = Request.Query.sort ?? null,
+                        SortDirection = Request.Query.sortdirection ?? null,
+                        MinumumPrice = Request.Query.minimumprice ?? Double.MinValue,
+                        MaximumPrice = Request.Query.maximumprice ?? Double.MaxValue,
+                        VeganOnly = Request.Query.veganonly ?? null,
+                        Category = string.IsNullOrEmpty(Request.Query.category) ? MenuCategory.All  : Enum.Parse(typeof(MenuCategory), Request.Query.category),
+                    };
+
+                    return GetMenu(cafeMenuQuery);
+                },
+                null,
+                "GetMenu");
+        }
+
+        [Route("GetMenu")]
+        [Route(HttpMethod.Get, "/menu")]
+        [Route(Summary = "Get Cafe Menu")]
+        [SwaggerResponse(HttpStatusCode.OK, Message = "OK")]
+        [Route(Tags = new[] { "Cafe Module" })]
+        private CafeMenu GetMenu([RouteParam(ParameterIn.Query, Name = "GetMenu")]CafeMenuQuery cafeMenuQuery)
+        {
+            return new CafeMenu
+            {
+                Items = new List<MenuItem>
+                {
+                    new MenuItem
+                    {
+                        Name = "Pesto Pasta",
+                        Price = 9.99,
+                        Category = MenuCategory.Food,
+                        IsVegan = false,
+                    },
+                    new MenuItem
+                    {
+                        Name = "Avocado Toast",
+                        Price = 8.99,
+                        Category = MenuCategory.Food,
+                        IsVegan = true,
+                    },
+                    new MenuItem
+                    {
+                        Name = "Tiramisu",
+                        Price = 5.99,
+                        Category = MenuCategory.Dessert,
+                        IsVegan = false,
+                    },
+                    new MenuItem
+                    {
+                        Name = "Flat white",
+                        Price = 3.99,
+                        Category = MenuCategory.Beverage,
+                        IsVegan = false,
+                    },
+                }
+            };
+        }
+    }
+}
